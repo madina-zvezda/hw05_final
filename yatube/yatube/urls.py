8fa4ddd21fe56de2d725172284545a06ce41aa18
Муминова Мадина <madi92@mail.ru>
2022-11-14 17:31:03 +0600

    первая итерация

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -4,6 +4,8 @@ from django.contrib import admin
 from django.urls import include, path
 
 handler404 = 'core.views.page_not_found'
+handler403 = 'core.views.csrf_failure'
+handler500 = 'core.views.server_error'
 urlpatterns = [
     path('auth/', include('users.urls', namespace='user')),
     path('auth/', include('django.contrib.auth.urls')),
