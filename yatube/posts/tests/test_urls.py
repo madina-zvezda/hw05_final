3c2b9ec8136ff2ba728ff05000080c2c40e49037
Муминова Мадина <madi92@mail.ru>
2022-11-14 17:36:13 +0600

    первая итерация

--- yatube/posts/tests/test_urls.py
+++ yatube/posts/tests/test_urls.py
@@ -103,19 +103,3 @@ class PostsURLTest(TestCase):
         self.assertEqual(response.status_code,
                          HTTPStatus.NOT_FOUND)
         self.assertTemplateUsed(response, 'core/404.html')
-
-    def test_403(self):
-        """Страница 403 отдаёт кастомный шаблон."""
-        response = self.guest_client.get('/test-not',
-                                         follow=True)
-        self.assertEqual(response.status_code,
-                         HTTPStatus.FORBIDDEN)
-        self.assertTemplateUsed(response, 'core/403.html')
-
-    def test_500(self):
-        """Страница 500 отдаёт кастомный шаблон."""
-        response = self.guest_client.get('/test-not',
-                                         follow=True)
-        self.assertEqual(response.status_code,
-                         HTTPStatus.INTERNAL_SERVER_ERROR)
-        self.assertTemplateUsed(response, 'core/500.html')
